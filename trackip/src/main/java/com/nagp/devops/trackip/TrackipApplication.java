e3c614cced511ba38467942f722ffc8f12dd6d70
Nimit Johri <nimit.johri@nagarro.com>
2020-07-27 01:32:02 +0530

    first commit

---
+++ trackip/src/main/java/com/nagp/devops/trackip/TrackipApplication.java
@@ -0,0 +1,13 @@
+package com.nagp.devops.trackip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackipApplication.class, args);
+	}
+
+}
---
+++ trackip/src/test/java/com/nagp/devops/trackip/TrackipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagp.devops.trackip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
